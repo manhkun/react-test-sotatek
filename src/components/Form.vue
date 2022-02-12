7d419a31cf96e853a80a8a3f774b7cee40a1071a
Quốc Mạnh Phùng <kunpro1st@gmail.com>
2022-02-12 14:02:27 +0700

    build: base style + base component

---
+++ src/components/Form.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="st-form">
+    <div class="st-form__title">
+      <input type="text" placeholder="Add new task">
+    </div>
+    <div class="st-form__desc">
+      <label for="st-form-description">Description</label>
+      <textarea name="description" id="st-form-description" cols="30" rows="10"></textarea>
+    </div>
+    <div class="st-form__bottom">
+      <div class="st-form__duedate">
+        <label for="st-form-duedate">Due date</label>
+        <input type="date" id="st-form-duedate">
+      </div>
+      <div class="st-form__priority">
+        <label for="">Priority</label>
+        <select name="priority" id="st-form-priority">
+          <option value="low">Low</option>
+          <option value="normal">Normal</option>
+          <option value="high">High</option>
+        </select>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+</script>
+
+<style lang="sass">
+
+</style>
---
+++ src/components/NewTask.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="st-newtask">
+    <div class="st-newtask__header st-header">
+      <h2>New Task</h2>
+    </div>
+
+    <div class="st-newtask__form">
+      <Form />
+    </div>
+  </div>
+</template>
+
+<script lang="ts" setup>
+import Form from './Form.vue'
+</script>
